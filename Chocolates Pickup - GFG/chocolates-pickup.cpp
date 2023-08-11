0cf44076ef2af045f1069002247b9af24e9b0ce1
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2023-08-11 15:01:47 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -32,16 +32,17 @@ class Solution {
     int solve(int m, int n, vector<vector<int>>& grid) {
         // code here
        
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,0)));
-        
+        //vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+        vector<vector<int>>prev(n,vector<int>(n,0));
         for(int j1=0;j1<n;j1++){
             for(int j2=0;j2<n;j2++){
-                if(j1==j2) dp[m-1][j1][j2]=grid[m-1][j1];
-                else dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+                if(j1==j2) prev[j1][j2]=grid[m-1][j1];
+                else prev[j1][j2]=grid[m-1][j1]+grid[m-1][j2];
             }
         }
         
         for(int i=m-2;i>=0;i--){
+            vector<vector<int>>curr(n,vector<int>(n,0));
             for(int j1=n-1;j1>=0;j1--){
                 for(int j2=n-1;j2>=0;j2--){
                      int maxi=0;
@@ -52,7 +53,7 @@ class Solution {
                         else value=grid[i][j1]+grid[i][j2];
                             
                         if(j1+d1<n&&j2+d2<n&&j1+d1>=0&&j2+d2>=0){
-                            value=value+dp[i+1][j1+d1][j2+d2];
+                            value=value+prev[j1+d1][j2+d2];
                         }
                         else value=-1e8;
                         
@@ -61,11 +62,12 @@ class Solution {
                             
             }
         }
-        dp[i][j1][j2]=maxi;
+        curr[j1][j2]=maxi;
                 }
             }
+            prev=curr;
         }
-        return dp[0][0][n-1];
+        return prev[0][n-1];
         
         // return rec(0,0,n-1,grid,dp);
     }
