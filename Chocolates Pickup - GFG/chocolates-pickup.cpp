21bf7f361a72af16f13acb74b36f27fab45983d0
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2023-08-11 14:52:49 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -32,9 +32,42 @@ class Solution {
     int solve(int m, int n, vector<vector<int>>& grid) {
         // code here
        
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,0)));
         
-        return rec(0,0,n-1,grid,dp);
+        for(int j1=0;j1<n;j1++){
+            for(int j2=0;j2<n;j2++){
+                if(j1==j2) dp[m-1][j1][j2]=grid[m-1][j1];
+                else dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+            }
+        }
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j1=n-1;j1>=0;j1--){
+                for(int j2=n-1;j2>=0;j2--){
+                     int maxi=0;
+                    for(int d1=-1;d1<=1;d1++){
+                     for(int d2=-1;d2<=1;d2++){
+                         int value=0;
+                         if(j1==j2) value=grid[i][j1];
+                        else value=grid[i][j1]+grid[i][j2];
+                            
+                        if(j1+d1<n&&j2+d2<n&&j1+d1>=0&&j2+d2>=0){
+                            value=value+dp[i+1][j1+d1][j2+d2];
+                        }
+                        else value=-1e8;
+                        
+                        maxi=max(maxi,value);
+                            
+                            
+            }
+        }
+        dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
+        
+        // return rec(0,0,n-1,grid,dp);
     }
 };
 
