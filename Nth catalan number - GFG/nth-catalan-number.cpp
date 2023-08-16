996640b6ad2af8ab018bed8d4f51f6b7ae5e1cfa
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2023-08-16 16:31:45 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.cpp
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -7,41 +7,29 @@ class Solution
 {
     public:
     //Function to find the nth catalan number.
-    int mod=1e9+7;
-    
-    
-    int rec(int n,vector<int>&dp){
-        if(n==1||n==0) {
-            return 1;
-        }
-        
-        if(dp[n]!=-1) return dp[n]%mod;
-        
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int a=rec(i,dp)%mod;
-            int b=rec(n-1-i,dp)%mod;
-            
-           sum=(sum+((a*b)%mod))%mod;
-        }
-        return dp[n]=sum%mod;
-    }
-    int findCatalan(int n) 
-    {
-        //code here
-       vector<int> dp(n + 1, 0);
-    dp[0] = dp[1] = 1;
+    const int mod = 1e9 + 7;
 
-    for (int i = 2; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            dp[i] = (dp[i] + (dp[j] * 1LL * dp[i - 1 - j]) % mod) % mod;
-        }
+int rec(int n, vector<int>& dp) {
+    if (n == 0) {
+        return 1;
     }
 
-    return dp[n];
-        
-        
+    if (dp[n] != -1) {
+        return dp[n];
     }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = (sum + (rec(i, dp) * 1LL * rec(n - 1 - i, dp)) % mod) % mod;
+    }
+
+    return dp[n] = sum;
+}
+
+int findCatalan(int n) {
+    vector<int> dp(n + 1, -1);
+    return rec(n, dp);
+}
 };
 
 //{ Driver Code Starts.
