b21a857902799192d5df7fea3fc06b850f969a9e
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-06-04 21:31:48 +0530

    Time: 7 ms (28.01%), Space: 12 MB (39.87%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
-    int check(vector<int>& arr, int a) {
-        int n = arr.size();
-        int start = 0, end = n - 1;
-        
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] < a) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+
+
+    int check(vector<int>& arr, int a){
+
+        int n=arr.size();
+        int res=-1;
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]>=a){
+                res=a;
+                end=mid-1;
             }
+            else start=mid+1;
         }
+
         
-        // Number of elements in the array that are less than `a`
-        return a - (start + 1);
+        return (a-res);
+
+
+        
     }
-    
-    int findKthPositive(vector<int>& arr, int k) {
-        int start = 1, end = 1e4;
-        int res = -1;
+    int findKthPositive(vector<int>& nums, int k) {
         
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            int missingCount = check(arr, mid);
-            
-            if (missingCount < k) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int n=nums.size();
+        int start=0,end=n-1;
+        int res=-1;
+
+        if(k<nums[0]) return k;
+        if(k>=nums[n-1]) return n+k;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(nums[mid]-mid-1>=k){
+                res=mid;
+                end=mid-1;
+            }
+           
+            else{
+                start=mid+1;
             }
         }
-        
-        // The k-th missing number
-        return start-1;
+
+        if(res==-1) return nums[n-1]+k-(nums[n-1]-(n-1)-1);
+        int ans=nums[res-1]+k-(nums[res-1]-(res-1)-1);
+        return ans;
     }
 };
