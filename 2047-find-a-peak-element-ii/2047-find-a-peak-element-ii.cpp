9e910f51fbbaf8c131e8b621f832909c92968ccf
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-06-08 01:17:08 +0530

    Time: 113 ms (92.9%), Space: 49.2 MB (7.15%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& v) {
-        int n = v.size();
-        int m = v[0].size();
-        int low = 0, high = m - 1;
+        
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int maxRow = 0;
+        int n=v.size();
+        int m=v[0].size();
 
-            // Find the maximum element in the middle column
-            for (int i = 0; i < n; i++) {
-                if (v[i][mid] > v[maxRow][mid]) {
-                    maxRow = i;
+        int start=0, end=m-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int mxvalue=-1;
+            int mx_ind=0;
+            for(int i=0;i<n;i++){
+                if(mxvalue<v[i][mid]){
+                    mxvalue=v[i][mid];
+                    mx_ind=i;
                 }
+
             }
 
-            // Compare the maximum element with its neighbors
-            bool leftIsBig = (mid - 1 >= 0 && v[maxRow][mid - 1] > v[maxRow][mid]);
-            bool rightIsBig = (mid + 1 < m && v[maxRow][mid + 1] > v[maxRow][mid]);
+             bool leftIsBig = (mid - 1 >= 0 && v[mx_ind][mid - 1] > v[mx_ind][mid]);
+            bool rightIsBig = (mid + 1 < m && v[mx_ind][mid + 1] > v[mx_ind][mid]);
 
             // If the middle element is greater than its neighbors, it is a peak
             if (!leftIsBig && !rightIsBig) {
-                return {maxRow, mid};
+                return {mx_ind, mid};
             } else if (leftIsBig) {
-                high = mid - 1; // Move to the left half
+                end = mid - 1; // Move to the left half
             } else {
-                low = mid + 1; // Move to the right half
+                start = mid + 1; // Move to the right half
             }
         }
         return {};
