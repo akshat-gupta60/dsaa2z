cd0cd8f7d4f0d40315e64d10244525703dd31dda
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-06-08 00:18:54 +0530

    Time: 50 ms (44.18%), Space: 17.3 MB (86.06%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& v, int ta) {
+         int n=v.size();
+        int m=v[0].size();
+        int i=0, j=m-1;
+        while(i>=0 && i<n && j>=0 && j<m){
+            int mid=v[i][j];
+
+            if(mid==ta) return true;
+            else if(mid<ta){
+                i++;
+            }
+            else if(mid>ta){
+                j--;
+            }
+        }
+        return false;
+    }
+};
