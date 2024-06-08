674cc1c8591c76f53c1f8ca63a559a8bc4fa5942
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-06-08 22:20:32 +0530

    Time: 3 ms (58.12%), Space: 8.5 MB (15.69%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        string st="";
+        int n=s.size();
+        int start_ind=0;
+        int is_plus=0;
+        for(int i=0;i<n;i++){
+            if(s[i]==' '){
+                continue;
+            }
+            
+            else{
+                start_ind=i;
+                break;
+            }
+        }
+        for(int i=start_ind;i<n;i++){
+            if(s[i]=='+'){
+                is_plus=is_plus+1;
+                continue;
+            }
+            
+            else{
+                start_ind=i;
+                break;
+            }
+        }
+
+
+        int is_neg=0;
+        if(s[start_ind]=='-'){
+            is_neg=is_neg+1;
+            start_ind++;
+            
+        }
+        if(is_neg>=1 && is_plus>=1) return 0;
+        if(is_neg>1|| is_plus>1) return 0; 
+        
+        
+        for(int i=start_ind;i<n;i++){
+            if(s[i]>='0' && s[i]<='9' ){
+                st.push_back(s[i]);
+            }
+            else break;
+        }
+        
+        long long int sum=0;
+        for(int i=0;i<st.size();i++){
+            sum=sum*10+(st[i]-'0');
+            if(sum>INT_MAX){
+                if(is_neg==1) return INT_MIN;
+                return INT_MAX;
+            }
+            
+        }
+        if(is_neg==1){
+            sum=(-1)*sum;
+        }
+        
+        if(sum>=INT_MIN && sum<=INT_MAX) return sum;
+        else if(sum<(INT_MIN)) return INT_MIN;
+        return INT_MAX;
+    
+
+
+
+
+
+
+
+    }
+};
