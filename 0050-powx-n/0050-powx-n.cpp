986af44cc16fb3479f0ec07f9fef3011e6710a74
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-07-09 15:50:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (93.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,13 +3,13 @@ public:
     double myPow(double x, int n) {
         
         if(x==1) return 1.00;
-        long long int n1=abs(n);
+        long long int n1=n;
 
-        
+        if(n<0) n1=(-1)*n1;
         double res=1;
 
         while(n1){
-            if(n1%2==0){
+            if((n1&1)==0){
                 x=x*x;
                 n1=n1/2;
             }
