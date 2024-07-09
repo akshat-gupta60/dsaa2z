7260377d16172f4c3979172f4805fda1e73fb979
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-07-09 20:11:03 +0530

    Time: 3 ms (38.38%), Space: 8.3 MB (12.08%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,38 +1,33 @@
-
-
-
 class MyStack {
 public:
-queue<int>q1,q2;
+ queue<int>q;
     MyStack() {
-        
+       
     }
     
     void push(int x) {
-        q2.push(x);
+        q.push(x);
 
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
+        for(int i=0;i<q.size()-1;i++){
+            q.push(q.front());
+            q.pop();
         }
 
-        swap(q1,q2);
+
     }
     
     int pop() {
-        
-            int a=q1.front();
-            q1.pop();
-            return a;
-        
+        int a=q.front();
+        q.pop();
+        return a;
     }
     
     int top() {
-        return q1.front();
+        return q.front();
     }
     
     bool empty() {
-        return (q1.size()==0);
+        return (q.size()==0);
     }
 };
 
