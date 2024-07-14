83ae6f0bf12ce607d30398dc7c9d0ba86ac4f0d8
Akshat <109871785+akshat-gupta60@users.noreply.github.com>
2024-07-14 20:08:06 +0530

    Time: 8 ms (73.36%), Space: 22.7 MB (44.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& v) {
-        vector<int>mxl,mxr;
         int n=v.size();
-        mxl.push_back(v[0]);
-        mxr.push_back(v[n-1]);
-
-        for(int i=1;i<n;i++){
-            int a=max(mxl[mxl.size()-1],v[i]);
-            mxl.push_back(a);
-        }
-
-        for(int i=n-2;i>=0;i--){
-            int a=max(mxr[mxr.size()-1],v[i]);
-            mxr.push_back(a);
-        }
-        reverse(mxr.begin(),mxr.end());
-
+        int left=0,right=n-1;
+        int left_max=0,right_max=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            ans=ans+ min(mxl[i],mxr[i])-v[i];
+        while(left<=right){
+
+            if(v[left]<=v[right]){
+                if(v[left]>left_max) left_max=v[left];
+                else {
+                    ans=ans+(left_max-v[left]);
+                }
+                left++;
+            }
+            else{
+                if(v[right]>right_max) right_max=v[right];
+                else{
+                    ans=ans+(right_max-v[right]);
+                }
+                right--;
+            }
         }
-
         return ans;
-
-
     }
 };
